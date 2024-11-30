14549b138aea98534209fe0f4fce2c270dc16035
joaquinrodriguezm7 <joaquinrodriguezm7@gmail.com>
2024-11-30 14:25:32 -0300

    Seguridad para ataques de fuerza bruta

--- interfaz/src/component/login.jsx
+++ interfaz/src/component/login.jsx
@@ -25,6 +25,9 @@ function Login() {
 
     const passwordIsInvalid = didEdit.password && values.password.trim().length < 6;
 
+    const [failedAttempts, setFailedAttempts] = useState(0);
+    const [isBlocked, setIsBlocked] = useState(false);
+    
    
     function validateForm() {
         const isEmailValid = values.email.trim() !== '' && values.email.includes('@');
@@ -43,6 +46,8 @@ function Login() {
         let pnombre = '';
         let message = '';
         const apiUrl = `${import.meta.env.VITE_API_URL}users`;
+
+
         try{
             
             const resp = await userApi.post(apiUrl, values, {
@@ -72,13 +77,25 @@ function Login() {
             if (error.request.status == 401) {
                 message = 'Email o Contraseña Invalida';
                 console.log(message)
+                setFailedAttempts(failedAttempts + 1);
+                
+                if (failedAttempts + 1 >= 3) {
+                    message = 'Demasiados intentos fallidos, \nintente nuevamente en 1 minuto';
+                    setIsBlocked(true);
+                    setTimeout(() => {
+                        setIsBlocked(false);
+                        setFailedAttempts(0);
+                    }, 60000);
+                }
+
+                setErrorMessage(message);
+                setShowMessage(true);
+                
             } else if (error.request.status == 500){
                 message = 'Error en el servidor'
             } else {
                 console.error('Unexpected error:', error);
             }
-            setErrorMessage(message);
-            setShowMessage(true);
         }
     }
 
@@ -159,7 +176,7 @@ function Login() {
                     </div>
                     <br />
                     <div className="btn-submit">
-                        <button disabled={!formValid} type='submit'>Iniciar Sesión</button>
+                        <button disabled={!formValid || isBlocked} type='submit'>Iniciar Sesión</button>
                     </div>
                 </form>
             </div>
